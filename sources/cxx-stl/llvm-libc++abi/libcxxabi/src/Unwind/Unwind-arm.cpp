f0ccc4a69f1ec3bd3055438b6131bf78d87067f1
Dana Jansens <danakj@google.com>
2013-11-21 22:07:07 -0500

    Make Unwind-arm.c into Unwind-arm.cpp
    Change-Id: I3344fa879ff533aab09eb4958f545b38c76a2675

--- sources/cxx-stl/llvm-libc++abi/libcxxabi/src/Unwind/Unwind-arm.cpp
+++ sources/cxx-stl/llvm-libc++abi/libcxxabi/src/Unwind/Unwind-arm.cpp
@@ -16,28 +16,32 @@
 #include <stdlib.h>
 
 #include "config.h"
+#include "UnwindCursor.hpp"
 
 #if __arm__ && !CXXABI_SJLJ
 
-_Unwind_Reason_Code __aeabi_unwind_cpp_pr0(_Unwind_State state,
-                                           _Unwind_Control_Block *ucbp,
-                                           _Unwind_Context *context) {
+extern "C" _Unwind_Reason_Code __aeabi_unwind_cpp_pr0(
+    _Unwind_State state,
+    _Unwind_Control_Block *ucbp,
+    _Unwind_Context *context) {
 #warning TODO(danakj): Support ARM/NEON here.
   assert(false && "pr0 not supported");
   return _URC_FAILURE;
 }
 
-_Unwind_Reason_Code __aeabi_unwind_cpp_pr1(_Unwind_State state,
-                                           _Unwind_Control_Block *ucbp,
-                                           _Unwind_Context *context) {
+extern "C" _Unwind_Reason_Code __aeabi_unwind_cpp_pr1(
+    _Unwind_State state,
+    _Unwind_Control_Block *ucbp,
+    _Unwind_Context *context) {
 #warning TODO(danakj): Support ARM/NEON here.
   assert(false && "pr1 not supported");
   return _URC_FAILURE;
 }
 
-_Unwind_Reason_Code __aeabi_unwind_cpp_pr2(_Unwind_State state,
-                                           _Unwind_Control_Block *ucbp,
-                                           _Unwind_Context *context) {
+extern "C" _Unwind_Reason_Code __aeabi_unwind_cpp_pr2(
+    _Unwind_State state,
+    _Unwind_Control_Block *ucbp,
+    _Unwind_Context *context) {
 #warning TODO(danakj): Support ARM/NEON here.
   assert(false && "pr2 not supported");
   return _URC_FAILURE;
