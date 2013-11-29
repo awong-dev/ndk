2716ec369396c9c90d77567ca16ca38dae91ec28
Albert J. Wong <ajwong@chromium.org>
2013-11-29 03:00:44 -0800

    Add ehabi flags when using llvm-3.4.
    llvm-3.3 doesn't support emitting ehabi descriptor tables.
    If using a bleeding edge 3.4 toolchain, add flags for these descriptors.
    
    Change-Id: I8f1996035465dc6ca795c0cec0c462a534c28165

--- build/tools/build-cxx-stl.sh
+++ build/tools/build-cxx-stl.sh
@@ -181,6 +181,9 @@ fi
 #COMMON_CFLAGS="-fPIC -O2 -ffunction-sections -fdata-sections -fcolor-diagnostics"
 COMMON_CFLAGS="-fPIC -O0 -g -ffunction-sections -fdata-sections -fcolor-diagnostics"
 COMMON_CXXFLAGS="-fexceptions -frtti -fuse-cxa-atexit"
+if [ "$LLVM_VERSION" = "3.4" ]; then
+  COMMON_CFLAGS="${COMMON_CFLAGS} -mllvm -arm-enable-ehabi-descriptors -mllvm -arm-enable-ehabi"
+fi
 
 if [ "$WITH_DEBUG_INFO" ]; then
     COMMON_CFLAGS="$COMMON_CFLAGS -g"
