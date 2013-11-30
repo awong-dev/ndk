540056dd8ed0a3a87711ec32be66a20d1a411715
Albert J. Wong <ajwong@chromium.org>
2013-11-30 02:49:38 -0800

    Revert "Redefine semantics of lsda for pr0,1,2 functions. Not sure this is right but it seems more consistent"
    This reverts commit c2692319e4a049fd97e9d109f0312c7827d4215f.

--- sources/cxx-stl/llvm-libc++abi/libcxxabi/src/Unwind/UnwindCursor.hpp
+++ sources/cxx-stl/llvm-libc++abi/libcxxabi/src/Unwind/UnwindCursor.hpp
@@ -655,13 +655,13 @@ bool UnwindCursor<A, R>::getInfoFromEHABISection(
   //   exceptionTableData -- the data inside the first word of the eht entry.
   //   isInlinedInIndex -- whether the entry is in the index.
   unw_word_t personalityRoutine = 0xbadf00d;
-  // Assume the languageSpecificDataAddr is the "other data" following the
-  // personality routing in the exception handler table (section 6.1 EHABI).
-  //
-  // If the personality routing is in compact form, meaning it is one of
-  // __aeabi_unwind_cpp_pr{0,1,2}, then the "other data" is just the unwind
-  // opcodes for those functions.  In this case, assume that the language
-  // specific data is just these unwind opcodes.
+  // The languageSpecificDataAddr should follow the personality function inside
+  // the exception handler table (section 6.1 EHABI). The personality function
+  // can either be a pointer (encoded as an offset) or it can be placed inline
+  // into the exception handler table (section 6.2 EHABI). Further, the
+  // personality function can be inlined into the index table, but in this case
+  // the entry is known to fit entirely in 4 bytes, so there can be no
+  // language-specific data (section 6.3 EHABI).
   unw_word_t languageSpecificDataAddr = 0xbadf00d;
 
   // If the high bit in the exception handling table entry is set, the entry is
@@ -674,21 +674,31 @@ bool UnwindCursor<A, R>::getInfoFromEHABISection(
       case 0:
         personalityRoutine = (unw_word_t) &__aeabi_unwind_cpp_pr0;
         extraWords = 0;
-        languageSpecificDataAddr = exceptionTableAddr + 1;
         break;
       case 1:
         personalityRoutine = (unw_word_t) &__aeabi_unwind_cpp_pr1;
         extraWords = (exceptionTableData & 0x00ff0000) >> 16;
-        languageSpecificDataAddr = exceptionTableAddr + 2;
         break;
       case 2:
         personalityRoutine = (unw_word_t) &__aeabi_unwind_cpp_pr2;
         extraWords = (exceptionTableData & 0x00ff0000) >> 16;
-        languageSpecificDataAddr = exceptionTableAddr + 2;
         break;
       default:
         _LIBUNWIND_ABORT("unknown personality routine");
         return false;
+    }
+
+    if (isInlinedInIndex) {
+      languageSpecificDataAddr = 0;
+      if (extraWords != 0) {
+        _LIBUNWIND_ABORT("index inlined table detected but pr function "
+                         "requires extra words");
+        return false;
+      }
+    } else  {
+      // Skip 4 bytes for initial data word then 4*extraWords depending on
+      // data for pr function.
+      languageSpecificDataAddr = exceptionTableAddr + 4 + 4 * extraWords;
     }
   } else {
     pint_t personalityAddr =
