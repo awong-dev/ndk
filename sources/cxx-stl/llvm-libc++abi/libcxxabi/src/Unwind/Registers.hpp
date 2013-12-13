35d5aef9635ab01067049821f3e4948b87392ea0
Albert J. Wong <ajwong@chromium.org>
2013-12-13 06:12:37 -0800

    Fix storage location for wMMX registers
    Change-Id: I5fb6cc431ebf660e79b80faa73a5bbd691e38ba1

--- sources/cxx-stl/llvm-libc++abi/libcxxabi/src/Unwind/Registers.hpp
+++ sources/cxx-stl/llvm-libc++abi/libcxxabi/src/Unwind/Registers.hpp
@@ -1323,6 +1323,8 @@ private:
   //
   // Per EHABI spec, the VRS only needs to store data for D0-D31.
   uint8_t _vfpRegisterData[sizeof(double) * 32];
+  uint8_t _wmmxData[sizeof(uint64_t) * 16];
+  uint8_t _wmmxControl[sizeof(uint32_t) * 4];
 };
 
 inline Registers_arm::Registers_arm(const void *registers) {
@@ -1331,11 +1333,15 @@ inline Registers_arm::Registers_arm(const void *registers) {
   // See unw_getcontext() note about data.
   memcpy(&_registers, registers, sizeof(_registers));
   bzero(_vfpRegisterData, sizeof(_vfpRegisterData));
+  bzero(_wmmxData, sizeof(_wmmxData));
+  bzero(_wmmxControl, sizeof(_wmmxControl));
 }
 
 inline Registers_arm::Registers_arm() {
   bzero(&_registers, sizeof(_registers));
   bzero(_vfpRegisterData, sizeof(_vfpRegisterData));
+  bzero(_wmmxData, sizeof(_wmmxData));
+  bzero(_wmmxControl, sizeof(_wmmxControl));
 }
 
 inline bool Registers_arm::validRegister(int regNum) const {
@@ -1545,10 +1551,12 @@ inline const char *Registers_arm::getRegisterName(int regNum) {
 }
 
 inline bool Registers_arm::validFloatRegister(int regNum) const {
-  // FIXME: Handle UNW_ARM_S[0-31] registers.
-  if ((regNum >= UNW_ARM_D0) && (regNum <= UNW_ARM_D31))
-    return true;
-  return false;
+  // NOTE: Consider the indel MMX registers floating points so the
+  // unw_get_fpreg can be used to transmit the 64-bit data back.
+  return ((regNum >= UNW_ARM_S0) && (regNum <= UNW_ARM_S31))
+      || ((regNum >= UNW_ARM_D0) && (regNum <= UNW_ARM_D31))
+      || ((regNum >= UNW_ARM_WR0) && (regNum <= UNW_ARM_WR15))
+      || ((regNum >= UNW_ARM_WC0) && (regNum <= UNW_ARM_WC3));
 }
 
 inline unw_fpreg_t Registers_arm::getFloatRegister(int regNum) const {
@@ -1564,11 +1572,11 @@ inline unw_fpreg_t Registers_arm::getFloatRegister(int regNum) const {
            sizeof(double));
   } else if (regNum >= UNW_ARM_WR0 && regNum <= UNW_ARM_WR15) {
     int index = regNum - UNW_ARM_WR0;
-    memcpy(value.bytes, &_vfpRegisterData[index * sizeof(uint64_t)],
+    memcpy(value.bytes, &_wmmxData[index * sizeof(uint64_t)],
            sizeof(uint64_t));
   } else if (regNum >= UNW_ARM_WC0 && regNum <= UNW_ARM_WC3) {
     int index = regNum - UNW_ARM_WC0;
-    memcpy(value.bytes, &_vfpRegisterData[index * sizeof(uint32_t)],
+    memcpy(value.bytes, &_wmmxControl[index * sizeof(uint32_t)],
            sizeof(uint32_t));
   }
   return value;
@@ -1586,11 +1594,11 @@ inline void Registers_arm::setFloatRegister(int regNum, unw_fpreg_t value) {
            sizeof(double));
   } else if (regNum >= UNW_ARM_WR0 && regNum <= UNW_ARM_WR15) {
     int index = regNum - UNW_ARM_WR0;
-    memcpy(&_vfpRegisterData[index * sizeof(uint64_t)], value.bytes,
+    memcpy(&_wmmxData[index * sizeof(uint64_t)], value.bytes,
            sizeof(uint64_t));
   } else if (regNum >= UNW_ARM_WC0 && regNum <= UNW_ARM_WC3) {
     int index = regNum - UNW_ARM_WC0;
-    memcpy(&_vfpRegisterData[index * sizeof(uint32_t)], value.bytes,
+    memcpy(&_wmmxControl[index * sizeof(uint32_t)], value.bytes,
            sizeof(uint32_t));
   }
 }
