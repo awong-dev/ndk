a6a8808e1b5d136a5e3233f1f46fe510ca383e8c
Jonathan Roelofs <jonathan@codesourcery.com>
2014-05-27 11:53:54 -0600

    Fix baremetal build

--- sources/cxx-stl/llvm-libc++abi/libcxxabi/test/buildandtest.sh
+++ sources/cxx-stl/llvm-libc++abi/libcxxabi/test/buildandtest.sh
@@ -9,10 +9,11 @@ export RANLIB=arm-none-eabi-ranlib
 export TRIPLE=armv4-none-eabi
 export CC=/Users/jroelofs/workdir/mentor/cs-llvm/build/Release+Asserts/bin/clang
 export CXX=/Users/jroelofs/workdir/mentor/cs-llvm/build/Release+Asserts/bin/clang++
+export SYSROOT=/Users/jroelofs/workdir/mentor/cs-llvm/build/Release+Asserts/SDKs/eabi-elf/codesourcery/arm
 rm libc++abi.a || true
 rm /Users/jroelofs/workdir/mentor/cs-llvm/build/Release+Asserts/SDKs/eabi-elf/codesourcery/arm/usr/lib/armv4t/libc++abi.a || true
 ./buildit
-cp libc++abi.a /Users/jroelofs/workdir/mentor/cs-llvm/libcxxabi/test/../../build/Release+Asserts/SDKs/eabi-elf/codesourcery/arm/usr/lib/armv4
+cp libc++abi.a /Users/jroelofs/workdir/mentor/cs-llvm/build/Release+Asserts/SDKs/eabi-elf/codesourcery/arm/usr/lib/armv4
 cp libc++abi.a /Users/jroelofs/workdir/mentor/cs-llvm/build/Release+Asserts/SDKs/eabi-elf/codesourcery/arm/usr/lib/armv4t
 
 cd ../test
