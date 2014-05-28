awong-dev/ndk

commit cfa0b15c097a4cda42897d5ee8de413c06625157

Fix wchart_t to not use intmax_t when in C99 mode
Change-Id: I62c137ae0f29dc2c3d16866044d2b8b9a8cfae2f

sources/android/support/include/wchar.h
@@ -162,8 +162,10 @@ unsigned long wcstoul (const wchar_t *__restrict__, wchar_t **__restrict__, int)
 
 long long wcstoll (const wchar_t *__restrict__, wchar_t **__restrict__, int);
 unsigned long long wcstoull (const wchar_t *__restrict__, wchar_t **__restrict__, int);
+#if defined(__cplusplus) && __cplusplus > 201103L
 intmax_t wcstoimax (const wchar_t * nptr, wchar_t** endptr , int base);
 uintmax_t wcstoumax (const wchar_t * nptr, wchar_t** endptr , int base);
+#endif
 
 
 int fwide (FILE *, int);

commit 8b8fd2c9704593aeb1777d84a2504e8ae03b1b0b

Fix depr.c.headers/inttypes_h.pass.cpp regression after cfa0b15c09.
cfa0b15c09 tried to disable wcstoimax in C99 mode, but accidentally
disabled it in C++11 mode too. Fix this, to make inttypes.h.pass.cpp
work again.

Change-Id: I81cbf7a855be4d9b87b8532740c2afe4a80887b4

sources/android/support/include/wchar.h
@@ -162,7 +162,7 @@ unsigned long wcstoul (const wchar_t *__restrict__, wchar_t **__restrict__, int)
 
 long long wcstoll (const wchar_t *__restrict__, wchar_t **__restrict__, int);
 unsigned long long wcstoull (const wchar_t *__restrict__, wchar_t **__restrict__, int);
-#if defined(__cplusplus) && __cplusplus > 201103L
+#if defined(__cplusplus) && __cplusplus >= 201103L
 intmax_t wcstoimax (const wchar_t * nptr, wchar_t** endptr , int base);
 uintmax_t wcstoumax (const wchar_t * nptr, wchar_t** endptr , int base);
 #endif