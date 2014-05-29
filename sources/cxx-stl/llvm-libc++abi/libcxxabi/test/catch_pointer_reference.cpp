1fdd586acbfe758929886f1c22c3ea4abed9a575
Jonathan Roelofs <jonathan@codesourcery.com>
2014-05-29 10:09:54 -0600

    Add test for catching pointer references

---
+++ sources/cxx-stl/llvm-libc++abi/libcxxabi/test/catch_pointer_reference.cpp
@@ -0,0 +1,84 @@
+//===---------------------- catch_class_04.cpp ----------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// Test case derived from: https://gcc.gnu.org/ml/gcc-patches/2009-08/msg00264.html
+//
+//===----------------------------------------------------------------------===//
+
+#include <exception>
+#include <stdlib.h>
+#include <assert.h>
+
+struct A {};
+struct B : A {};
+
+void f1()
+{
+    B b;
+    throw &b;
+    assert(false);
+}
+
+int main()
+{
+    try
+    {
+        f1();
+        assert(false);
+    }
+    catch (A *a)
+    {
+        assert(true);
+    }
+    catch (...)
+    {
+        assert(false);
+    }
+
+    try
+    {
+        f1();
+        assert(false);
+    }
+    catch (B *b)
+    {
+        assert(true);
+    }
+    catch (...)
+    {
+        assert(false);
+    }
+    try
+    {
+        f1();
+        assert(false);
+    }
+    catch (A *&a)
+    {
+        assert(true);
+    }
+    catch (...)
+    {
+        assert(false);
+    }
+
+    try
+    {
+        f1();
+        assert(false);
+    }
+    catch (B *&b)
+    {
+        assert(true);
+    }
+    catch (...)
+    {
+        assert(false);
+    }
+}
