97fba118d4b97cf0dfab42d71ebcd586ed81fb0c
Jonathan Roelofs <jonathan@codesourcery.com>
2014-05-30 10:47:29 -0600

    Clean up catch_pointer_reference, and add more comments

--- sources/cxx-stl/llvm-libc++abi/libcxxabi/test/catch_pointer_reference.cpp
+++ sources/cxx-stl/llvm-libc++abi/libcxxabi/test/catch_pointer_reference.cpp
@@ -6,6 +6,24 @@
 // Source Licenses. See LICENSE.TXT for details.
 //
 //===----------------------------------------------------------------------===//
+//
+//  This test case checks specifically the cases under bullet 3.1 & 3.2:
+//
+//  C++ ABI 15.3:
+//  A handler is a match for an exception object of type E if
+//     *  The handler is of type cv T or cv T& and E and T are the same type
+//        (ignoring the top-level cv-qualifiers), or
+//     *  the handler is of type cv T or cv T& and T is an unambiguous base
+//        class of E, or
+//     *  the handler is of type cv1 T* cv2 and E is a pointer type that can
+//        be converted to the type of the handler by either or both of
+//          o  a standard pointer conversion (4.10 [conv.ptr]) not involving
+//             conversions to private or protected ambiguous classes
+//          o  a qualification conversion
+//     *  the handler is a pointer or pointer to member type and E is
+//        std::nullptr_t
+//
+//===----------------------------------------------------------------------===//
 
 #include <exception>
 #include <stdlib.h>
@@ -15,17 +33,17 @@
 struct Base {};
 struct Derived : Base {};
 
-template <typename HandlerType, typename ThrowType>
+template <typename T, typename E>
 void assert_catches()
 {
     try
     {
         Derived d;
-        throw static_cast<ThrowType>(&d);
+        throw static_cast<E>(&d);
         printf("%s\n", __PRETTY_FUNCTION__);
         assert(false && "Statements after throw must be unreachable");
     }
-    catch (HandlerType h)
+    catch (T t)
     {
         assert(true);
         return;
@@ -40,17 +58,17 @@ void assert_catches()
     assert(false && "The catch should have returned");
 }
 
-template <typename HandlerType, typename ThrowType>
+template <typename T, typename E>
 void assert_cannot_catch()
 {
     try
     {
         Derived d;
-        throw static_cast<ThrowType>(&d);
+        throw static_cast<E>(&d);
         printf("%s\n", __PRETTY_FUNCTION__);
         assert(false && "Statements after throw must be unreachable");
     }
-    catch (HandlerType h)
+    catch (T t)
     {
         printf("%s\n", __PRETTY_FUNCTION__);
         assert(false && "Should not have entered the catch");
@@ -139,22 +157,20 @@ void f4()
 
 void f5()
 {
-    // Note that it is a C++ ABI defect that these do catch
-    //
     // Test that every combination of handler of type:
-    //   cv1 Base * cv2 &
+    //   cv1 Derived * cv2 &
     // catches an exception of type:
     //   Derived *
-    assert_catches<               Base *                &, Derived *>();
-    assert_catches<const          Base *                &, Derived *>();
-    assert_catches<      volatile Base *                &, Derived *>();
-    assert_catches<const volatile Base *                &, Derived *>();
-    assert_catches<               Base * const          &, Derived *>();
-    assert_catches<               Base * const volatile &, Derived *>();
-    assert_catches<const          Base * const          &, Derived *>();
-    assert_catches<const          Base * const volatile &, Derived *>();
-    assert_catches<const volatile Base * const          &, Derived *>();
-    assert_catches<const volatile Base * const volatile &, Derived *>();
+    assert_catches<               Derived *                &, Derived *>();
+    assert_catches<const          Derived *                &, Derived *>();
+    assert_catches<      volatile Derived *                &, Derived *>();
+    assert_catches<const volatile Derived *                &, Derived *>();
+    assert_catches<               Derived * const          &, Derived *>();
+    assert_catches<               Derived * const volatile &, Derived *>();
+    assert_catches<const          Derived * const          &, Derived *>();
+    assert_catches<const          Derived * const volatile &, Derived *>();
+    assert_catches<const volatile Derived * const          &, Derived *>();
+    assert_catches<const volatile Derived * const volatile &, Derived *>();
 }
 
 void f6()
@@ -195,58 +211,40 @@ void f7()
 
 void f8()
 {
-    // Test case derived from the discussion here:
+    // This test case has a caveat noted in the discussion here:
     //   https://gcc.gnu.org/ml/gcc-patches/2009-08/msg00264.html
     // Specifically:
-    //   "This [test exposes a] corner case of the ARM C++ ABI. The generic C++
-    //    ABI also gets this wrong, because I failed to notice the subtlety here.
-    //     The issue is that 15.3/3 3rd bullet says:
-    //        "the handler is of type cv1 T* cv2 and E is a pointer type that
-    //         can be converted to the type of the handler by either or both of:
-    //         a standard pointer conversion (4.10) not involving conversions to
-    //         pointers to private or protected or ambiguous classes"
-    //    Notice that the handlers of type "cv1 T*cv2&" are not allowed such
-    //    freedom to find a base class. The ABI error is that we treat handlers
-    //    of reference type exactly the same as the corresponding hander of
-    //    non-reference type. Elsewhere in the exception handling this makes no
-    //    difference (for instance bullet 1 explicitly says 'cv T or cv T&')."
+    //   This [test exposes a] corner case of the ARM C++ ABI. The generic C++
+    //   ABI also gets this wrong, because I failed to notice the subtlety here.
+    //   The issue is that 15.3/3 3rd bullet says:
+    //     The handler is of type cv1 T* cv2 and E is a pointer type that
+    //     can be converted to the type of the handler by either or both of:
+    //       * a standard pointer conversion (4.10) not involving conversions
+    //         to pointers to private or protected or ambiguous classes
+    //   Notice that the handlers of type "cv1 T*cv2&" are not allowed such
+    //   freedom to find a base class. The ABI error is that we treat handlers
+    //   of reference type exactly the same as the corresponding hander of
+    //   non-reference type. Elsewhere in the exception handling this makes no
+    //   difference (for instance bullet 1 explicitly says 'cv T or cv T&').
     //
     // See also: http://www.open-std.org/jtc1/sc22/wg21/docs/cwg_defects.html#388
     //
-    //  TL;DR: it is a C++ ABI defect that these do catch in the genetic C++ ABI
-    //         but ARM EABI gets it right, and there, these should not catch.
+    //  TL;DR: it is an unresolved C++ ABI defect that these do catch
 
-#if !defined(__ARM_EABI__)
     // Test that every combination of handler of type:
-    //   cv1 Derived * cv2 &
+    //   cv1 Base * cv2 &
     // catches an exception of type:
     //   Derived *
-    assert_catches<               Derived *                &, Derived *>();
-    assert_catches<const          Derived *                &, Derived *>();
-    assert_catches<      volatile Derived *                &, Derived *>();
-    assert_catches<const volatile Derived *                &, Derived *>();
-    assert_catches<               Derived * const          &, Derived *>();
-    assert_catches<               Derived * const volatile &, Derived *>();
-    assert_catches<const          Derived * const          &, Derived *>();
-    assert_catches<const          Derived * const volatile &, Derived *>();
-    assert_catches<const volatile Derived * const          &, Derived *>();
-    assert_catches<const volatile Derived * const volatile &, Derived *>();
-#else // defined(__ARM_EABI__)
-    // Test that every combination of handler of type:
-    //   cv1 Derived * cv2 &
-    // cannot catch an exception of type:
-    //   Derived *
-    assert_cannot_catch<               Derived *                &, Derived *>();
-    assert_cannot_catch<const          Derived *                &, Derived *>();
-    assert_cannot_catch<      volatile Derived *                &, Derived *>();
-    assert_cannot_catch<const volatile Derived *                &, Derived *>();
-    assert_cannot_catch<               Derived * const          &, Derived *>();
-    assert_cannot_catch<               Derived * const volatile &, Derived *>();
-    assert_cannot_catch<const          Derived * const          &, Derived *>();
-    assert_cannot_catch<const          Derived * const volatile &, Derived *>();
-    assert_cannot_catch<const volatile Derived * const          &, Derived *>();
-    assert_cannot_catch<const volatile Derived * const volatile &, Derived *>();
-#endif // defined(__ARM_EABI__)
+    assert_catches<               Base *                &, Derived *>();
+    assert_catches<const          Base *                &, Derived *>();
+    assert_catches<      volatile Base *                &, Derived *>();
+    assert_catches<const volatile Base *                &, Derived *>();
+    assert_catches<               Base * const          &, Derived *>();
+    assert_catches<               Base * const volatile &, Derived *>();
+    assert_catches<const          Base * const          &, Derived *>();
+    assert_catches<const          Base * const volatile &, Derived *>();
+    assert_catches<const volatile Base * const          &, Derived *>();
+    assert_catches<const volatile Base * const volatile &, Derived *>();
 }
 
 int main()
