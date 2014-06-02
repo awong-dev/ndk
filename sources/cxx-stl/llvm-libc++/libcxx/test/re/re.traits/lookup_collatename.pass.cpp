c97e3d69f2d07add2e49570741d642764cf066d0
Philippe Liard <pliard@chromium.org>
2014-06-02 19:05:23 +0200

    Move failing parts of lookup_collatename to a separate file instead.
    Change-Id: I8f1ce8dcca604e75cd8dbdffe20da9fd8bed1496

--- sources/cxx-stl/llvm-libc++/libcxx/test/re/re.traits/lookup_collatename.pass.cpp
+++ sources/cxx-stl/llvm-libc++/libcxx/test/re/re.traits/lookup_collatename.pass.cpp
@@ -104,12 +104,6 @@ int main()
     test("tild", std::string(""));
     test("ch", std::string(""));
 
-#if !defined(__ANDROID__)
-    std::locale::global(std::locale("cs_CZ.ISO8859-2"));
-    test("ch", std::string("ch"));
-    std::locale::global(std::locale("C"));
-#endif
-
     test(L"NUL", std::wstring(L"\x00", 1));
     test(L"alert", std::wstring(L"\x07"));
     test(L"backspace", std::wstring(L"\x08"));
@@ -182,10 +176,4 @@ int main()
 
     test(L"tild", std::wstring(L""));
     test(L"ch", std::wstring(L""));
-
-#if !defined(__ANDROID__)
-    std::locale::global(std::locale("cs_CZ.ISO8859-2"));
-    test(L"ch", std::wstring(L"ch"));
-    std::locale::global(std::locale("C"));
-#endif
 }
